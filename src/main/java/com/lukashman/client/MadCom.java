b9bea3b75ddb87daa82e9f6994c995f5dca8b6f5
Lukashman <shram191@rambler.ru>
2017-03-08 15:05:31 +0300

    Add buttons

--- src/main/java/com/lukashman/client/MadCom.java
+++ src/main/java/com/lukashman/client/MadCom.java
@@ -1,5 +1,7 @@
 package com.lukashman.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -7,18 +9,25 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class MadCom implements EntryPoint {
 	
-	private VerticalPanel vp = new VerticalPanel();
 	private Button ha = new Button("Hank: Wiki Quote");
 	private Button ja = new Button("Jebus: Wiki Quote");
 	private Button tr = new Button("Tricky: Wiki Quote");
 	private Button au = new Button("Auditor: Wiki Quote");
 	
+	
+	
 	public void onModuleLoad() {
-		vp.add(ha);
-		ha.getElement().setId("hav");
-		RootPanel.get("vab").add(vp);
-		
-		
+		setAll();
 		RootPanel.getBodyElement().setId("bod");
+	}
+	
+	public void setAll(){
+		Button[] buts = {ha,ja,tr,au};
+		String[] di = {"vab","vac","vax","vaz"};
+		
+		for(int i=0;i<=3;i++){
+			buts[i].getElement().setId("hav");
+			RootPanel.get(di[i]).add(buts[i]);
+		}
 	}
 }
